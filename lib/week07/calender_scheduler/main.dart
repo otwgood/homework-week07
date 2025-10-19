7a8346e2ad96857ca0294f53afe2ff384530c611
otwgood <otwgood@naver.com>
2025-10-19 15:27:16 +0900

    .

--- lib/week07/calender_scheduler/main.dart
+++ lib/week07/calender_scheduler/main.dart
@@ -3,3 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:myapp/week07/calender_scheduler/model/drift_database.dart';
 import 'package:get_it/get_it.dart';
+
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
+}
