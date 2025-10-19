fa1a044f9793ae93a87d2336aeef9ca75e9cbd35
otwgood <otwgood@naver.com>
2025-10-19 22:06:50 +0900

    ..

--- lib/week07/calender_scheduler/screen/home_screen.dart
+++ lib/week07/calender_scheduler/screen/home_screen.dart
@@ -10,3 +10,34 @@ import 'package:myapp/week07/calender_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+lass HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key : key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(  
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: PRIMARY_COLOR,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isDismissible: true,
+            builder: (_) => ScheduleBottomSheet(
+              selectedDate: selectedDate,
+            ),
+            isScrollControlled: true,
+          );
+       
