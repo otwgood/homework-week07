0c5564f606b9240b793b751552b1d134822244cb
otwgood <otwgood@naver.com>
2025-10-19 15:20:56 +0900

    aa

---
+++ lib/week07/calender_scheduler/component/custom_text_field.dart
@@ -0,0 +1,54 @@
+import 'package:myapp/week05/calender_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final bool isTime;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+
+  const CustomTextField({
+    required this.label,
+    required this.isTime,
+    required this.onSaved,
+    required this.validator,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(      // 세로로 텍스트와 텍스트 필드 배치
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [  
+        Text(  
+          label,
+          style: TextStyle(  
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Expanded(  
+          flex: isTime ? 0 : 1,
+          child: TextFormField(  
+            onSaved: onSaved,
+            validator: validator,
+            cursorColor: Colors.grey,
+            maxLines: isTime ? 1 : null,
+
+            expands: !isTime,
+            keyboardType: isTime ? TextInputType.number : TextInputType.multiline,
+            inputFormatters: isTime ? [FilteringTextInputFormatter.digitsOnly,] 
+              : [],
+            decoration: InputDecoration(  
+              border: InputBorder.none,
+              filled: true,
+              fillColor: Colors.grey[300],
+              suffixText: isTime ? '시' : null,
+            ),
+          ),
+        ),        
+      ],
+    );  
+  }
+}
---
+++ lib/week07/calender_scheduler/main.dart
@@ -0,0 +1,6 @@
+import 'package:myapp/week07/calender_scheduler/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:myapp/week07/calender_scheduler/model/drift_database.dart';
+import 'package:get_it/get_it.dart';
+
