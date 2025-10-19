659a68aaae8753e86d54d5956d4eae7eeb326b85
otwgood <otwgood@naver.com>
2025-10-19 16:00:00 +0900

    .

--- lib/week07/calender_scheduler/component/main_calendar.dart
+++ lib/week07/calender_scheduler/component/main_calendar.dart
@@ -1,2 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.d
+import 'package:table_calendar/table_calendar.dart';
+import 'package:myapp/week07/calender_scheduler/const/colors.dart';
+
+class MainCalendar extends StatelessWidget {
+  final OnDaySelected onDaySelected;
+  final DateTime selectedDate;
+
+  MainCalendar({
+    required this.onDaySelected,
+    required this.selectedDate,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(  
+      locale: 'ko_kr',
+      onDaySelected: onDaySelected,
+      selectedDayPredicate: (date) =>
+        date.year == selectedDate.year &&
+        date.month == selectedDate.month &&
