1f2cfe4ce8db5d73150d151c577395f3d177dcc3
otwgood <otwgood@naver.com>
2025-10-19 16:08:42 +0900

    .

--- lib/week07/calender_scheduler/component/main_calendar.dart
+++ lib/week07/calender_scheduler/component/main_calendar.dart
@@ -19,3 +19,34 @@ class MainCalendar extends StatelessWidget {
       selectedDayPredicate: (date) =>
         date.year == selectedDate.year &&
         date.month == selectedDate.month &&
+        date.day == selectedDate.day,        
+
+      firstDay: DateTime(1800, 1, ),
+      lastDay: DateTime(3000, 1, 1),
+      focusedDay: DateTime.now(),
+      
+      headerStyle: HeaderStyle(
+        titleCentered: true,
+        formatButtonVisible: false,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
+      ),
+      calendarStyle: CalendarStyle(  
+        isTodayHighlighted: false,
+        defaultDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6.0),
+          color: LIGHT_GREY_COLOR,
+        ),
+        weekendDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6.0),
+          color: LIGHT_GREY_COLOR,
+        ),
+        selectedDecoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6.0),
+          border: Border.all(  
+            color: PRIMARY_COLOR,
+            width: 1.0,
+          ),
+        ),
