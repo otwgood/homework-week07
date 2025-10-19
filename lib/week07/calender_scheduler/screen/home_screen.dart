fc8c42bc8d23160e179033df68f8361f63acbc3d
otwgood <otwgood@naver.com>
2025-10-19 22:07:03 +0900

    ..

--- lib/week07/calender_scheduler/screen/home_screen.dart
+++ lib/week07/calender_scheduler/screen/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:get_it/get_it.dart';
 import 'package:myapp/week07/calender_scheduler/model/drift_database.dart';
 
@@ -10,7 +9,7 @@ import 'package:myapp/week07/calender_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-lass HomeScreen extends StatefulWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key : key);
 
   @override
@@ -40,4 +39,72 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             isScrollControlled: true,
           );
-       
+        },
+        child: Icon(  
+          Icons.add,
+        ),
+      ),
+
+      body: SafeArea(
+        child: Column(
+          children: [  
+            MainCalendar(
+              selectedDate: selectedDate,         
+              onDaySelected: onDaySelected,            
+            ),
+            SizedBox(height: 8.0),
+            StreamBuilder<List<Schedule>>(
+            stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+            builder: (context, snapshot) {
+              return TodayBanner(
+                selectedDate: selectedDate,
+                count: snapshot.data?.length ?? 0,
+              );
+            }),
+            SizedBox(height: 8.0,),
+            Expanded(
+              child: StreamBuilder<List<Schedule>>(
+                stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Container();
+                  }
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      final schedule = snapshot.data![index];
+                      
+                      return Dismissible(
+                        key: ObjectKey(schedule.id),
+                        direction: DismissDirection.startToEnd,
+                        onDismissed: (DismissDirection direction) {
+                          GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
+                        },
+
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
+                        child: ScheduleCard(
+                          startTime: schedule.startTime,
+                          endTime: schedule.endTime,
+                          content: schedule.content,
+                        ),
+                      ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
+    setState(() { 
+      this.selectedDate = selectedDate;
+    });
+  }
+}
